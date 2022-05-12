99d356e6a6920516e19ecce28dbcfa9343a9d5b3
Oliver Lundin <79059027+olvard@users.noreply.github.com>
2022-05-12 11:43:07 +0200

    topten

--- src/App.js
+++ src/App.js
@@ -9,7 +9,10 @@ import {
   PresentationControls,
   OrbitControls,
   Cloud,
-  Stars, Sky, ScrollControls, Scroll
+  Stars,
+  Sky,
+  ScrollControls,
+  Scroll,
 } from '@react-three/drei'
 import Car from './components/Car'
 import RotationWrapper from './components/RotationWrapper'
@@ -19,43 +22,49 @@ const MyMesh = () => {
 
   useFrame(({ clock }) => {
     const a = Math.sin(clock.getElapsedTime())
-    refMesh.current.rotation.y = a
-    refMesh.current.rotation.x = a
-    refMesh.current.rotation.z = a
-    
+    // refMesh.current.rotation.y = a
+    // refMesh.current.rotation.x = a
+    // refMesh.current.rotation.z = a
   })
   return (
-    <mesh ref={refMesh}>
-      <boxGeometry></boxGeometry>
-      <meshLambertMaterial color={'red'}></meshLambertMaterial>
-    </mesh>
+    <group
+      onWheel={(e) => {
+        let a = document.body.scrollTop
+        let b = document.body.scrollHeight - document.body.clientHeight
+        let c = a / b
+        console.log(c)
+        refMesh.current.rotation.y = c
+      }}
+    >
+      <mesh ref={refMesh}>
+        <boxGeometry></boxGeometry>
+        <meshLambertMaterial color={'red'}></meshLambertMaterial>
+      </mesh>
+    </group>
   )
 }
 
 export default function App() {
-  const scroll = useRef(0);
+  const scroll = useRef(0)
   return (
-    <Canvas dpr={[1, 2]} shadows camera={{ position: [0, 5, 15], fov: 45 }}>
+    <Canvas dpr={[1, 2]} shadows camera={{ fov: 45 }}>
       {/* <OrbitControls /> */}
       <color attach='background' args={['#101010']} />
       <fog attach='fog' args={['#101010', 10, 20]} />
       <Suspense fallback={null}>
-
-
         <Environment path='/cube' />
-        
+
         {/* <PresentationControls speed={1.5} global zoom={1} polar={[-0.1, Math.PI / 4]} snap={true} > */}
 
         <Stage environment={null} intensity={1} contactShadow={false} shadowBias={-0.0015}>
-
-        <RotationWrapper scroll={scroll}>
-        <Car rotation={[0,0,0]} />
-        </RotationWrapper>
-        
+          <RotationWrapper scroll={scroll}>
+            {/* <Car rotation={[0, 0, 0]} /> */}
+            <MyMesh />
+          </RotationWrapper>
         </Stage>
-        
+
         <mesh rotation={[-Math.PI / 2, 0, 0]}>
-          <planeGeometry args={[170, 170]} /> 
+          <planeGeometry args={[170, 170]} />
           <MeshReflectorMaterial
             blur={[300, 100]}
             resolution={2048}
@@ -69,7 +78,7 @@ export default function App() {
             metalness={0.5}
           />
         </mesh>
-        
+
         {/* </PresentationControls> */}
       </Suspense>
     </Canvas>
--- src/components/RotationWrapper.js
+++ src/components/RotationWrapper.js
@@ -1,23 +1,21 @@
-import { useFrame } from '@react-three/fiber';
-import React, { useRef } from 'react';
-import * as THREE from 'three';
+import { useFrame } from '@react-three/fiber'
+import React, { useRef } from 'react'
+import * as THREE from 'three'
 
 export default function RotationWrapper(props) {
   // This reference will give us direct access to the THREE.Mesh object
-  const group = useRef();
+  const group = useRef()
 
   // Subscribe this component to the render-loop, rotate the mesh every frame
   useFrame((state, delta) => {
-    group.current.rotation.y = THREE.MathUtils.degToRad((state.clock.elapsedTime * 360) / 1)
+    group.current.rotation.y = THREE.MathUtils.degToRad((props.scroll.current * 360) / 1)
     // console.log(state.clock.elapsedTime)
     // console.log(props)
   })
 
   return (
     <group ref={group} {...props} dispose={null}>
-      <group position={[0, 0, 0]}>
-          {props.children}
-      </group>
+      <group position={[0, 0, 0]}>{props.children}</group>
     </group>
   )
 }
